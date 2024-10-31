451170ef149a8f4f7419f6c1cc740ac9bf85c9c9
Glopark <glopark@192.168.1.44>
2024-10-31 16:23:36 +0300

    country data is fetched || now countries can be seen and selected

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,24 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_country_dropdown_app/home_page_service.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with HomePageService {
+
+   // list that will store all the country data
+  List countries = [];
+
+  // variable that wil store the country that is selected by the user
+  String? selectedCountry; 
+
+  // the method that fetches the country data will be executed before the home page widget is rendered
+  @override
+  void initState() {
+    super.initState();
+    fetchCountries();
+  }
+
+  // method to fetch the country data
+  Future fetchCountries() async {
+    final response = await http.get(Uri.parse('https://restcountries.com/v3.1/all'));
+    if(response.statusCode == 200) {
+      List data = jsonDecode(response.body);
+      setState(() {
+        countries = data.map((country) => country['name']['common']).toList();
+      });
+    } else {
+      throw Exception('Failed to load the country data.');
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SizedBox(
-              width: 250,
-              child: DropdownButtonFormField(
-                hint: const Text('Select a country'),
-                items: const [], 
-                onChanged: (value) => '',
+      body: countries.isEmpty 
+      ? const Center(child: CircularProgressIndicator())
+      : Center(
+        child: SizedBox(
+          width: 400,
+          child: DropdownButtonFormField<String>(
+            focusColor: Colors.transparent,
+            value: selectedCountry,
+            decoration: const InputDecoration(labelText: 'Select a country'),
+            onChanged: (newValue) {
+              setState(() {
+                selectedCountry = newValue;
+              });
+            },
+            items: countries.map<DropdownMenuItem<String>>(
+              (country) => DropdownMenuItem(
+                value: country,
+                child: Text(country), 
               ),
-            ),
-          ],
+            ).toList(),
+          ),
         ),
       ),
     );
